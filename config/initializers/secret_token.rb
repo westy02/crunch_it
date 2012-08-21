08542699c31a30c74c2e74de620ac7a44ad2e645
Glenn West <westy02@gmail.com>
2012-08-21 12:49:34 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrunchIt::Application.config.secret_token = '65b998b54ce88d847af78c29fc0a29d4a84538806e800904b4e3a328236bc93c8bdc6462fea91b780505a521179f22d3ce95a1c9dd732e9631a95a3505914eab'
