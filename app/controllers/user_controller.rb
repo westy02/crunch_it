0afd3b402a81417ed234ef648be9a5e620b9ecd9
westy02 <westy02@gmail.com>
2012-08-27 23:13:44 +1200

    Tabs to user home

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,18 @@ class UserController < ApplicationController
   def home
     if signed_in?
       daily_jobs
+    end
   end
-end
+  
+  def day
+    
+  end 
+  
+  def month
+    
+  end 
+  
+  def list
+    
+  end 
 end
