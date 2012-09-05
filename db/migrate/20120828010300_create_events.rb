d6eb3c94c341bbe5cb113dc0f0366cd1d5bebf9b
westy02 <westy02@gmail.com>
2012-09-05 14:50:25 +1200

    migration tweaks

--- db/migrate/20120828010300_create_events.rb
+++ db/migrate/20120828010300_create_events.rb
@@ -2,10 +2,8 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :name
-      t.date :start_at
-      t.date :end_at
-      t.time  :start_time
-      t.time  :end_time
+      t.datetime :start_at
+      t.datetime :end_at
       t.integer :user_id
       t.integer :job_id
 
