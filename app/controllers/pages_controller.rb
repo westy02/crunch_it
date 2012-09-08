25df777f479bdc2bc75d6db257074eef807022bc
westy02 <westy02@gmail.com>
2012-09-08 22:19:27 +1200

    daily event date correction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,9 +11,9 @@ class PagesController < ApplicationController
   def home  
     @jobs = current_user.jobs.all
     @events = current_user.events.order("start_at")
-    @daily_events = current_user.events.order("start_at").where("start_at = ?", params[:date])
     @events_by_date = @events.group_by(&:start_at)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @daily_events = current_user.events.order("start_at").where(:events => {:start_at => @date})
     end
   end
     
